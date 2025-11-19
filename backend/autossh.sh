edaca90450b16555d4768e62c75503498b4f60cc
winger <funnywwh@qq.com>
2025-11-19 08:18:06 +0800

    chore: 优化autossh配置
    - 更新AUTOSSH_GATETIME配置
    - 添加SSH连接保活和超时配置
    - 优化端口转发参数

--- backend/autossh.sh
+++ backend/autossh.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-export AUTOSSH_POLL=60
-autossh -M 0 -f -N -R 8080:localhost:8080 aiweb
+export AUTOSSH_GATETIME=0
+
+autossh -f -M 0 \
+        -o "ServerAliveInterval=15" \
+        -o "ServerAliveCountMax=3" \
+        -o "ConnectTimeout=10" \
+        -NT -R 0.0.0.0:8080:127.0.0.1:8080 \
+        aiweb
